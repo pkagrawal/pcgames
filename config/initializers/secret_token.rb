4239b7036d2b5a8ac9c4f9c32112e884e9c1a00d
Pradyumna Agrawal <agrawal.pradyumna@gmail.com>
2011-09-15 22:22:09 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pcgames::Application.config.secret_token = '79fe82b5c56ef31990ba0d3142520a0c54aa021bf2d70ad3d3a75a2f153148a02c327cb5f658f340f47191f342972f8e9bbe2f4a2016e0ef31dbcfb5ae544183'
